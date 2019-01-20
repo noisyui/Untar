dfd44fc8135fe9f08d0c710806a010e2c8516f1a
noisyui <wxf4work@gmail.com>
2019-01-21 00:39:32 +0800

    move cksum to file type

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 let decompress = require('decompress');
-let tar = require('tar');
-let fs = require('fs');
-let Buffer = require('buffer').Buffer;
 
 let untar = async (tarFileName) => {
-    let buffer = Buffer.alloc(512);
-    let fd = fs.openSync(tarFileName, 'r');
-    fs.readSync(fd, buffer, 0, 512, 0);
-
-    addMagicIfPrePOSIXTar(buffer);
-
-    // todo need write permission
-    // fs.writeSync(fd, buffer, 0, 512, 0);
-
     let tarBuffer = await decompress(tarFileName, 'output');
     return tarBuffer;
-};
-
-let addMagicIfPrePOSIXTar = (buffer) => {
-    let header = new tar.Header(buffer);
-    if (header.cksumValid) {
-        buffer.writeUInt8(0x75 ,257);
-        buffer.writeUInt8(0x73 ,258);
-        buffer.writeUInt8(0x74 ,259);
-        buffer.writeUInt8(0x61 ,260);
-        buffer.writeUInt8(0x72 ,261);
-    }
 };
 
 let tarBuffer =  untar('RAP2.TAR');
